967a145e439dc9c48edba19d814f85623c329a88
satyam <satyamlkr2004@gmail.com>
2025-06-16 13:42:03 +0530

    pep classess

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+console.log("hello from js1");
+console.log(age)
+//let const or var
+var age=30;//variable decleration && initialization
+console.log(age);
+var age=44//re-decleration
+console.log(age);
+*/
+
+//++++++++++
+/* in case of var we have to access the variable before declaration and it is re-declere*/
+//values is assign during the execuetion
+
+
+
+////but in case of the let we do not access before declaration and re-declaration in not allouded in let
+/*
+console.log(age);
+let age=22
+console.log(age)
+*/
+
+//operator in javascript
+//arithmetic operator +,-,*,,/
+
+//arithmetic operator
+
+
+
+
+
+
+
+
